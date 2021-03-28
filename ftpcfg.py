3404fd20457504a5a4f90d78fa004c0b57686e1c
le0nklcpp <le0nklcpp@gmail.com>
2021-03-28 17:55:58 +0300

    fix ftpcfg values conversion add new value pwhash make ftpserver use some variables from config

--- ftpcfg.py
+++ ftpcfg.py
@@ -15,27 +15,33 @@ class FTPjsconf:
  integrity_check = 0
  integrity_alg = ""
  default_dir = "files"
- motd = "FTPenstein"
+ motd = "FTPEnstein"
  port = 0
  max_cons = 50
  max_cons_per_ip = 25
+ pwhash=""
  def __init__(self,home):
-  self.data = json.load(open(home+'ftp.json','r'))
-  d = self.data
+  def bc(s):
+   return bool(int(s))
+  d = json.load(open(home+'ftp.json','r'))
+  # security object
   s = d['security']
   tls = s['tls']
-  self.tls_enabled = bool(tls['enabled'])
-  self.tls_force = bool(tls['force'])
+  self.tls_enabled = bc(tls['enabled'])
+  self.tls_force = bc(tls['force'])
   self.tls_cert = tls['cert']
   self.tls_key = tls['key']
   self.fails2ban = int(s['fails_before_punishment'])
   self.bancooldown = int(s['fails_cooldown_min'])
   self.bantime = int(s['punishment_time'])
   self.bantype = s['punishment']
-  self.anonymous = bool(s['allow_anonymous_rdonly'])
+  self.anonymous = bc(s['allow_anonymous_rdonly'])
+  self.pwhash = s['pw_hash_algorithm']
+  # integrity check object
   i = d['integrity_check']
   self.integrity_check = i['enabled']
   self.integrity_alg = i['algorithm']
+  # everything else
   self.default_dir = d['ftp_default_dir']
   self.motd = d['motd']
   self.port = int(d['port'])
--- ftpserver.py
+++ ftpserver.py
@@ -1,29 +1,45 @@
 # -*- coding: utf-8 -*-
 import pyftpdlib
+import ftpcfg
 from pyftpdlib.servers import FTPServer as srv
 from pyftpdlib.authorizers import DummyAuthorizer
-from pyftpdlib.handlers import TLS_FTPHandler
 import os.path
 import ftpusers
-# main function
+
+confp = './'
 def main():
+ print('FTPEnstein')
+ config = ftpcfg.FTPjsconf(confp)
+ print('Read json config')
  users=ftpusers.loadadmins()
- print("FTPServer")
  print('Loaded list of {0} users'.format(len(users)))
- authorizer = DummyAuthorizer()
+ authorizer = DummyAuthorizer() # TODO: write your own authorizer
+ print('Initialized authorizer')
  for i in users:
   if not os.path.exists(i[2]) or not os.path.isdir(i[2]):
-   i[2] = default_ftp_dir
+   i[2] = config.default_dir
   authorizer.add_user(i[0],i[1],i[2],perm='elradfmw')
- handler=TLS_FTPHandler
- handler.certfile='ftpuser.pem'
- handler.keyfile='ftpuserkey.pem'
- handler.authorizer=authorizer
+ # Anonymous read-only user
+ if config.anonymous:
+   print('Adding read-only anonymous user')
+   authorizer.add_anonymous(config.default_dir,perm='elr')
+ if config.tls_enabled:
+  print('TLS enabled')
+  from pyftpdlib.handlers import TLS_FTPHandler
+  handler=TLS_FTPHandler
+  handler.certfile=config.tls_cert
+  handler.keyfile=config.tls_key
+  handler.tls_control.required = handler.tls_data_required = config.tls_force
+  handler.authorizer=authorizer
+ else:
+  print('TLS disabled')
+  from pyftpdlib.handlers import FTPHandler
+  handler = FTPHandler
  handler.banner='Python FTP server'
- address=('','22545')
+ address=('',config.port)
  server=srv(address,handler)
- server.max_cons = 100
- server.max_cons_per_ip = 10
+ server.max_cons = config.max_cons
+ server.max_cons_per_ip = config.max_cons_per_ip
  server.serve_forever()
 if __name__=="__main__":
  main()
